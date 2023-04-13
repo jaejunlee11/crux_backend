7e3af2274b68c190f9e479c13918707a33450872
jaejunlee11 <sktks11@naver.com>
2023-04-13 16:09:37 +0000

    post를 통해서 데이터 전송, id값을 통해서 원하는 데이터만 가져오기 (확인 완료)

--- api/views.py
+++ api/views.py
@@ -11,13 +11,6 @@ from .serializer import *
 def API(request):
     return Response("test")
 
-# 재준 : db데이터를 가져오는 GET 생성
-# id값을 활용하여 여기서 사용 가능(test용으로 여기서는 사용 X)
-@api_view(['GET'])
-def getDB(request,id):
-    permissions=AuthGroupPermissions.objects.all()
-    return Response(permissions)
-
 
 # 재준 : db데이터를 가져오는 것 확인 (그냥 가져오는 경우 jason type이 아니라 오류 발생)
 # 재준 : serializer를 통해서 jason으로 변경 후 가져옴
@@ -26,3 +19,28 @@ def getTest(request):
     test=Test.objects.all()
     serializer = TestSerializer(test, many=True)
     return Response(serializer.data)
+
+# 재준 : db데이터를 가져오는 GET 생성 원하는 id와 일치하는 것만 가져옴(테스트 완료)
+# id값을 활용하여 id가 일치하는 정보만 GET해옴(성공)
+@api_view(['GET'])
+def getDB(request,id):
+    test=Test.objects.filter(id=id)
+    serializer = TestSerializer(test, many=True)
+    return Response(serializer.data)
+
+# 재준 : db에 데이터를 넣는 것 테스트 확인 완료
+# 해당 틀에 맞춰서 작성 해줘야함
+"""
+{
+"id" : 2,
+"name" : "jaejun"
+}
+"""
+@api_view(['POST'])
+def postTest(request):
+    reqData = request.data
+    serializer = TestSerializer(data=reqData)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
