5da06db0d01aa5986b3c0ffff783c9cdc046e293
jaejunlee11 <sktks11@naver.com>
2023-04-13 14:25:11 +0000

    DB에서 GET을 통해 가져오기 완료 api 확인

---
+++ api/serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .views import *
+
+# 재준 : jason 파일로 바꿔주기 위해서 serializer 생성
+class TestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Test
+        fields = '__all__'
+        
--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path, include
-from .views import API
-# 재준 : django 프로젝트 생성 api와 연결되는 uri생성
+from .views import *
+
+from django.contrib import admin
+
+# python3 manage.py runserver 0.0.0.0:8000 서버 실행
+
+# 재준 : django 프로젝트 생성 api와 연결되는 url생성
+# 재준 : db와 연결되는 api url생성
 urlpatterns = [
-    path("api/", API)
+    path("api/", API),
+    # 숫자에 따라서 다른 값을 받고 싶은 경우 int:id로 path 정의 하기
+    path("<int:id>",GetDB),
+    path("test/",GetTest),
 ]
+
