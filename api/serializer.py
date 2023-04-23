6e1f3eb93753c96270a267026d896e7d1f55b701
jaejunlee11 <sktks11@naver.com>
2023-04-23 04:59:45 +0000

    재준: 기능구현시 필요 부분 DB 연결(필요 데이터 정제는 아직)

--- api/serializer.py
+++ api/serializer.py
@@ -6,6 +6,29 @@ class TestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Test
         fields = '__all__'
+
+# 재준 : 클라이밍장 검색 화면, 섹터 선택 화면, 문제 선택 화면, 동영상 확인 화면 구현을 위한 serializer생성
+# 재준 : CruxClimbingspot, CruxQuestion, CruxSector, CruxVideo
+class CruxClimbingspotSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CruxClimbingspot
+        fields = '__all__'
+      
+class CruxQuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CruxQuestion
+        fields = '__all__'
+
+class CruxSectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CruxSector
+        fields = '__all__'
+
+class CruxVideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CruxVideo 
+        fields = '__all__'
+    
         
 class ForumPostSerializer(serializers.ModelSerializer):
     class Meta:
